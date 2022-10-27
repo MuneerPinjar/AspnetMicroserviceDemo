ee0c2de249e961669c46b315c3481228534d248e
Muneer Pinjar <Muneer.Pinjar.ext@unit4.com>
2022-10-27 16:30:05 +0530

    Catlog api microservice

---
+++ src/Services/Catlog/Catlog.API/Data/ICatlogContext.cs
@@ -0,0 +1,14 @@
+﻿using Catlog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catlog.API.Data
+{
+    public interface ICatlogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
